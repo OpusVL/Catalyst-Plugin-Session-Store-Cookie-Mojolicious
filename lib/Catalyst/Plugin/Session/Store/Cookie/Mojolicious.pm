21a7453de444fb7a5534ddd0c79b0a7dcfe0f1a4
Alastair Douglas <alastair.douglas@gmail.com>
2018-10-23 17:28:03 +0100

    Sort of works but not really

--- lib/Catalyst/Plugin/Session/Store/Cookie/Mojolicious.pm
+++ lib/Catalyst/Plugin/Session/Store/Cookie/Mojolicious.pm
@@ -1,10 +1,14 @@
 package Catalyst::Plugin::Session::Store::Cookie::Mojolicious;
 use strict;
 use warnings;
+use v5.24;
+
+# ABSTRACT: Cross-compatibility with Mojolicious session cookies
 
 use Moose;
 use Mojo::Util qw(b64_decode b64_encode);
 use Mojo::JSON;
+use Scalar::IfDefined qw/$ifdef/;
 
 extends 'Catalyst::Plugin::Session::Store';
 with 'Catalyst::ClassData';
@@ -17,29 +21,40 @@ our $VERSION = '0.001';
 __PACKAGE__->mk_classdata($_)
   for qw/_store_cookie_name _store_cookie_expires _secret/;
 
+use Data::Dump 'pp';
+
 sub get_session_data {
     my $self = shift;
     my $key = shift;
+    $key =~ s/:.+//;
 
+    say "Retrieve session data $key";
     $self->_needs_early_session_finalization(1);
     my $cookie = $self->req->cookie($self->_store_cookie_name);
-    my $value = $cookie->value;
+    my $value = $cookie->$ifdef('value');
 
-    if (defined $value and $value =~ s/--([^\-]+)$//) {
-        my $signature = $1;
-        my $check = $self->_checksum($value);
-        unless (Mojo::Util::secure_compare($check, $signature)) {
-            $self->log->debug("Cookie " . $self->_store_cookie_name . " has a bad signature");
-            undef $value;
+    if (defined $value) {
+        say "Found cookie $value";
+        if ($value =~ s/--([^\-]+)$//) {
+            my $signature = $1;
+            my $check = $self->_checksum($value);
+            unless (Mojo::Util::secure_compare($check, $signature)) {
+                $self->log->debug("Cookie " . $self->_store_cookie_name . " has a bad signature");
+                undef $value;
+            }
+        }
+        else {
+            $self->log->debug("Cookie " . $self->_store_cookie_name . " has no signature");
         }
     }
     else {
-        $self->log->debug("Cookie " . $self->_store_cookie_name . " has no signature");
+        $self->log->debug("Cookie " . $self->_store_cookie_name . " not set");
     }
 
     $self->{__cookie_session_store_cache__} =
         $self->_decode_session_data($value) // {};
 
+    say "Deserialised: " . pp $self->{__cookie_session_store_cache__};
     return $self->{__cookie_session_store_cache__}->{$key};
 }
 
@@ -47,23 +62,20 @@ sub store_session_data {
     my $self = shift;
     my $key  = shift;
     my $data = shift;
+    $key =~ s/:.+//;
 
     $self->{__cookie_session_store_cache__}->{$key} = $data;
 
+    say "Store session data $key = " . pp $data;
+
     my $options = {
         expires  => $self->_store_cookie_expires,
-# I don't know how Catalyst was setting these in the first place, if at all
-# but they are not mentioned in Catalyst::Plugin::Session::Store::Cookie
-#        domain   => $self->cookie_domain,
-#        httponly => 1,
-#        path     => $self->cookie_path,
-#        secure   => $self->secure
     };
 
     my $enc = $self->_encode_session_data($self->{__cookie_session_store_cache__}, $options);
     my $checksum = $self->_checksum($enc);
     my $value = "$enc--$checksum";
-    $self->res->cookies{$self->cookie_name} = {
+    $self->res->cookies->{$self->_store_cookie_name} = {
         value => $value,
         %$options
     };
@@ -72,6 +84,7 @@ sub store_session_data {
 
 sub delete_session_data {
     my ($self, $key) = @_;
+    $key =~ s/:.+//;
     delete $self->{__cookie_session_store_cache__}->{$key};
 }
 
